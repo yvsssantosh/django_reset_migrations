f0fe80433e8e3866509e3e041706724c6c7abc72
yvsssantosh <yvss.santosh@gmail.com>
2017-08-21 23:26:26 +0530

    Changed print statements to STD OUT

--- reset_migrations/management/commands/reset_migrations.py
+++ reset_migrations/management/commands/reset_migrations.py
@@ -1,5 +1,3 @@
-from __future__ import print_function
-
 from django.core.management import BaseCommand
 from django.core.management import call_command
 from django.db import connection
@@ -8,13 +6,13 @@ import shutil
 import re
 import tempfile
 
-def delete_line(filename, pattern):
+def delete_line(filename, pattern, stdout):
     pattern_compiled = re.compile(pattern)
     with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp_file:
         with open(filename) as src_file:
             for line in src_file:
                 if pattern_compiled.findall(line):
-                    print_function('Deleting line in %s' % filename)
+                    stdout.write('Deleting line in %s' % filename)
                     continue
                 tmp_file.write(line)
 
@@ -26,6 +24,7 @@ def delete_line(filename, pattern):
 
 class Command(BaseCommand):
     help = "Delete all migrations from one app, reset database and create one new migration"
+
 
     def __init__(self, *args, **kwargs):
         self.cursor = connection.cursor()
@@ -64,7 +63,7 @@ class Command(BaseCommand):
                                 continue
                             if '.py' in file_name:
                                 regex = r'\(\'%s\'' % app
-                                delete_line(file_name, regex)
+                                delete_line(file_name, regex, self.stdout)
 
     def handle(self, *args, **options):
         apps = options['apps']
